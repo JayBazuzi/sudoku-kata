6668ea7f929f0e775f9f84af3e5bb6520770bc69
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-03 16:41:57 +0000

    r   name anonymous type

--- SudokuKata/SudokuKata/Program.cs
+++ SudokuKata/SudokuKata/Program.cs
@@ -35,6 +35,34 @@ namespace SudokuKata
         }
     }
 
+    public class Applesauce2
+    {
+        public int Mask { get; set; }
+        public int Discriminator { get; set; }
+        public string Description { get; set; }
+        public IGrouping<int, Applesauce1> Cells { get; set; }
+
+        public override string ToString()
+        {
+            return $"{{ Mask = {Mask}, Discriminator = {Discriminator}, Description = {Description}, Cells = {Cells} }}";
+        }
+
+        public override bool Equals(object value)
+        {
+            var type = value as Applesauce2;
+            return (type != null) && EqualityComparer<int>.Default.Equals(type.Mask, Mask) && EqualityComparer<int>.Default.Equals(type.Discriminator, Discriminator) && EqualityComparer<string>.Default.Equals(type.Description, Description) && EqualityComparer<IGrouping<int, Applesauce1>>.Default.Equals(type.Cells, Cells);
+        }
+
+        public override int GetHashCode()
+        {
+            int num = 0x7a2f0b42;
+            num = (-1521134295 * num) + EqualityComparer<int>.Default.GetHashCode(Mask);
+            num = (-1521134295 * num) + EqualityComparer<int>.Default.GetHashCode(Discriminator);
+            num = (-1521134295 * num) + EqualityComparer<string>.Default.GetHashCode(Description);
+            return (-1521134295 * num) + EqualityComparer<IGrouping<int, Applesauce1>>.Default.GetHashCode(Cells);
+        }
+    }
+
     public class Program
     {
         public static void Play()
@@ -534,11 +562,9 @@ namespace SudokuKata
                                         .Where(group => @group.Count(tuple => candidateMasks[tuple.Index] == mask) == 2)
                                         .Where(group => @group.Any(tuple =>
                                             candidateMasks[tuple.Index] != mask && (candidateMasks[tuple.Index] & mask) > 0))
-                                        .Select(group => new
+                                        .Select(group => new Applesauce2
                                         {
-                                            Mask = mask,
-                                            Discriminator = @group.Key,
-                                            Description = @group.First().Description,
+                                            Mask = mask, Discriminator = @group.Key, Description = @group.First().Description,
                                             Cells = @group
                                         }))
                                 .ToList();
