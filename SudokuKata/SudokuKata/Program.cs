0601cffaa318966a2c3a7b79c5442e2e877c9705
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-03 16:42:42 +0000

    r   name anonymous type

--- SudokuKata/SudokuKata/Program.cs
+++ SudokuKata/SudokuKata/Program.cs
@@ -63,6 +63,36 @@ namespace SudokuKata
         }
     }
 
+    public class Applesauce3
+    {
+        public int Mask { get; set; }
+        public string Description { get; set; }
+        public IGrouping<int, Applesauce1> Cells { get; set; }
+        public List<Applesauce1> CellsWithMask { get; set; }
+        public int CleanableCellsCount { get; set; }
+
+        public override string ToString()
+        {
+            return $"{{ Mask = {Mask}, Description = {Description}, Cells = {Cells}, CellsWithMask = {CellsWithMask}, CleanableCellsCount = {CleanableCellsCount} }}";
+        }
+
+        public override bool Equals(object value)
+        {
+            var type = value as Applesauce3;
+            return (type != null) && EqualityComparer<int>.Default.Equals(type.Mask, Mask) && EqualityComparer<string>.Default.Equals(type.Description, Description) && EqualityComparer<IGrouping<int, Applesauce1>>.Default.Equals(type.Cells, Cells) && EqualityComparer<List<Applesauce1>>.Default.Equals(type.CellsWithMask, CellsWithMask) && EqualityComparer<int>.Default.Equals(type.CleanableCellsCount, CleanableCellsCount);
+        }
+
+        public override int GetHashCode()
+        {
+            int num = 0x7a2f0b42;
+            num = (-1521134295 * num) + EqualityComparer<int>.Default.GetHashCode(Mask);
+            num = (-1521134295 * num) + EqualityComparer<string>.Default.GetHashCode(Description);
+            num = (-1521134295 * num) + EqualityComparer<IGrouping<int, Applesauce1>>.Default.GetHashCode(Cells);
+            num = (-1521134295 * num) + EqualityComparer<List<Applesauce1>>.Default.GetHashCode(CellsWithMask);
+            return (-1521134295 * num) + EqualityComparer<int>.Default.GetHashCode(CleanableCellsCount);
+        }
+    }
+
     public class Program
     {
         public static void Play()
@@ -657,20 +687,16 @@ namespace SudokuKata
                                     cellGroups
                                         .Where(group => @group.All(cell =>
                                             state[cell.Index] == 0 || (mask & (1 << (state[cell.Index] - 1))) == 0))
-                                        .Select(group => new
+                                        .Select(group => new Applesauce3
                                         {
-                                            Mask = mask,
-                                            Description = @group.First().Description,
-                                            Cells = @group,
-                                            CellsWithMask =
-                                                @group.Where(cell =>
-                                                        state[cell.Index] == 0 && (candidateMasks[cell.Index] & mask) != 0)
-                                                    .ToList(),
-                                            CleanableCellsCount =
-                                                @group.Count(
-                                                    cell => state[cell.Index] == 0 &&
-                                                            (candidateMasks[cell.Index] & mask) != 0 &&
-                                                            (candidateMasks[cell.Index] & ~mask) != 0)
+                                            Mask = mask, Description = @group.First().Description, Cells = @group,
+                                            CellsWithMask = @group.Where(cell =>
+                                                    state[cell.Index] == 0 && (candidateMasks[cell.Index] & mask) != 0)
+                                                .ToList(),
+                                            CleanableCellsCount = @group.Count(
+                                                cell => state[cell.Index] == 0 &&
+                                                        (candidateMasks[cell.Index] & mask) != 0 &&
+                                                        (candidateMasks[cell.Index] & ~mask) != 0)
                                         }))
                                 .Where(group => @group.CellsWithMask.Count() == maskToOnesCount[@group.Mask])
                                 .ToList();
