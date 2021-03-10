33a18fab2b4744e8109ce54943bb9844e9a9b4cc
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-10 17:16:09 +0000

    r   move classes to files

---
+++ SudokuKata/SudokuKata/Stacks.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace SudokuKata
+{
+    public class Stacks
+    {
+        public Stacks()
+        {
+            // Top elements are (row, col) of cell which has been modified compared to previous state
+            var rowIndexStack = new Stack<int>();
+            var colIndexStack = new Stack<int>();
+
+            // Top element indicates candidate digits (those with False) for (row, col)
+            var usedDigitsStack = new Stack<bool[]>();
+
+            // Top element is the value that was set on (row, col)
+            var lastDigitStack = new Stack<int>();
+
+            RowIndexStack = rowIndexStack;
+            ColIndexStack = colIndexStack;
+            UsedDigitsStack = usedDigitsStack;
+            LastDigitStack = lastDigitStack;
+        }
+
+        public Stack<int> RowIndexStack { get; private set; }
+        public Stack<int> ColIndexStack { get; private set; }
+        public Stack<bool[]> UsedDigitsStack { get; private set; }
+        public Stack<int> LastDigitStack { get; private set; }
+
+    }
+}
--- SudokuKata/SudokuKata/SudokuBoardAndStackState.cs
+++ SudokuKata/SudokuKata/SudokuBoardAndStackState.cs
@@ -4,54 +4,6 @@ using System.Linq;
 
 namespace SudokuKata
 {
-    public class ViableMove
-    {
-        public ViableMove(int rowToWrite, int colToWrite, bool[] usedDigits, int[] currentState, int currentStateIndex,
-            int movedToDigit)
-        {
-            RowToWrite = rowToWrite;
-            ColToWrite = colToWrite;
-            UsedDigits = usedDigits;
-            CurrentState = currentState;
-            CurrentStateIndex = currentStateIndex;
-            MovedToDigit = movedToDigit;
-        }
-
-        public int RowToWrite { get; private set; }
-        public int ColToWrite { get; private set; }
-        public bool[] UsedDigits { get; private set; }
-        public int[] CurrentState { get; private set; }
-        public int CurrentStateIndex { get; private set; }
-        public int MovedToDigit { get; private set; }
-    }
-
-    public class Stacks
-    {
-        public Stacks()
-        {
-            // Top elements are (row, col) of cell which has been modified compared to previous state
-            var rowIndexStack = new Stack<int>();
-            var colIndexStack = new Stack<int>();
-
-            // Top element indicates candidate digits (those with False) for (row, col)
-            var usedDigitsStack = new Stack<bool[]>();
-
-            // Top element is the value that was set on (row, col)
-            var lastDigitStack = new Stack<int>();
-
-            RowIndexStack = rowIndexStack;
-            ColIndexStack = colIndexStack;
-            UsedDigitsStack = usedDigitsStack;
-            LastDigitStack = lastDigitStack;
-        }
-
-        public Stack<int> RowIndexStack { get; private set; }
-        public Stack<int> ColIndexStack { get; private set; }
-        public Stack<bool[]> UsedDigitsStack { get; private set; }
-        public Stack<int> LastDigitStack { get; private set; }
-
-    }
-
     public class SudokuBoardAndStackState
     {
         private const int Unknown = -1;
