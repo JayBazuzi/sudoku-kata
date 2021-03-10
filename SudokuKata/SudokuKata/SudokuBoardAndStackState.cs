964b2b57da186cb941ad0a4d12a3c94a6641d252
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-10 16:43:51 +0000

    r   transform parameters - outs to class

--- SudokuKata/SudokuKata/SudokuBoardAndStackState.cs
+++ SudokuKata/SudokuKata/SudokuBoardAndStackState.cs
@@ -4,6 +4,26 @@ using System.Linq;
 
 namespace SudokuKata
 {
+    public class ViableMove
+    {
+        public ViableMove(int rowToWrite, int colToWrite, bool[] usedDigits, int[] currentState, int currentStateIndex, int movedToDigit)
+        {
+            RowToWrite = rowToWrite;
+            ColToWrite = colToWrite;
+            UsedDigits = usedDigits;
+            CurrentState = currentState;
+            CurrentStateIndex = currentStateIndex;
+            MovedToDigit = movedToDigit;
+        }
+
+        public int RowToWrite { get; private set; }
+        public int ColToWrite { get; private set; }
+        public bool[] UsedDigits { get; private set; }
+        public int[] CurrentState { get; private set; }
+        public int CurrentStateIndex { get; private set; }
+        public int MovedToDigit { get; private set; }
+    }
+
     public class SudokuBoardAndStackState
     {
         public SudokuBoardAndStackState()
@@ -99,7 +119,13 @@ namespace SudokuKata
             Stack<int> colIndexStack, Stack<bool[]> usedDigitsStack,
             Stack<int> lastDigitStack, char[][] board)
         {
-            GetViableMove(stateStack, rowIndexStack, colIndexStack, usedDigitsStack, lastDigitStack, board, out var rowToWrite, out var colToWrite, out var usedDigits, out var currentState, out var currentStateIndex, out var movedToDigit);
+            var viableMove = GetViableMove(stateStack, rowIndexStack, colIndexStack, usedDigitsStack, lastDigitStack, board);
+            var rowToWrite = viableMove.RowToWrite;
+            var colToWrite = viableMove.ColToWrite;
+            var usedDigits = viableMove.UsedDigits;
+            var currentState = viableMove.CurrentState;
+            var currentStateIndex = viableMove.CurrentStateIndex;
+            var movedToDigit = viableMove.MovedToDigit;
 
             if (movedToDigit <= 9)
             {
@@ -120,22 +146,21 @@ namespace SudokuKata
             }
         }
 
-        private static void GetViableMove(Stack<int[]> stateStack, Stack<int> rowIndexStack, Stack<int> colIndexStack, Stack<bool[]> usedDigitsStack,
-            Stack<int> lastDigitStack, char[][] board, out int rowToWrite, out int colToWrite, out bool[] usedDigits,
-            out int[] currentState, out int currentStateIndex, out int movedToDigit)
+        private static ViableMove GetViableMove(Stack<int[]> stateStack, Stack<int> rowIndexStack, Stack<int> colIndexStack, Stack<bool[]> usedDigitsStack,
+            Stack<int> lastDigitStack, char[][] board)
         {
             var rowToMove = rowIndexStack.Peek();
             var colToMove = colIndexStack.Peek();
             var digitToMove = lastDigitStack.Pop();
 
-            rowToWrite = rowToMove + rowToMove / 3 + 1;
-            colToWrite = colToMove + colToMove / 3 + 1;
+            var rowToWrite = rowToMove + rowToMove / 3 + 1;
+            var colToWrite = colToMove + colToMove / 3 + 1;
 
-            usedDigits = usedDigitsStack.Peek();
-            currentState = stateStack.Peek();
-            currentStateIndex = 9 * rowToMove + colToMove;
+            var usedDigits = usedDigitsStack.Peek();
+            var currentState = stateStack.Peek();
+            var currentStateIndex = 9 * rowToMove + colToMove;
 
-            movedToDigit = digitToMove + 1;
+            var movedToDigit = digitToMove + 1;
             while (movedToDigit <= 9 && usedDigits[movedToDigit - 1])
             {
                 movedToDigit += 1;
@@ -147,6 +172,8 @@ namespace SudokuKata
                 currentState[currentStateIndex] = 0;
                 board[rowToWrite][colToWrite] = '.';
             }
+
+            return new ViableMove(rowToWrite, colToWrite, usedDigits, currentState, currentStateIndex, movedToDigit);
         }
 
         private static Command Applesauce_Collapse(Stack<int[]> stateStack, Stack<int> rowIndexStack,
