ba8b67cb1f8102a344e2e0feeb1fdde2760e5096
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-10 17:02:08 +0000

    r   make method non-static

--- SudokuKata/SudokuKata/SudokuBoardAndStackState.cs
+++ SudokuKata/SudokuKata/SudokuBoardAndStackState.cs
@@ -200,7 +200,7 @@ namespace SudokuKata
                         lastDigitStack.Push(viableMove.MovedToDigit);
                         viableMove.UsedDigits[viableMove.MovedToDigit - 1] = true;
                         viableMove.CurrentState[viableMove.CurrentStateIndex] = viableMove.MovedToDigit;
-                        SetValue(sudokuBoardAndStackState, viableMove.RowToWrite, viableMove.ColToWrite, viableMove.MovedToDigit);
+                        sudokuBoardAndStackState.SetValue(viableMove.RowToWrite, viableMove.ColToWrite, viableMove.MovedToDigit);
 
                         // Next possible digit was found at current position
                         // Next step will be to expand the state
@@ -218,9 +218,9 @@ namespace SudokuKata
             }
         }
 
-        private static void SetValue(SudokuBoardAndStackState board, int row, int column, int value)
+        public void SetValue(int row, int column, int value)
         {
-            board.Board[row][column] = (char) ('0' + value);
+            Board[row][column] = (char) ('0' + value);
         }
 
         private static ViableMove GetViableMove(Stack<int[]> stateStack, Stack<int> rowIndexStack,
