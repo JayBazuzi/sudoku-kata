d50df2e22a017655a02312acf5a1097556f71a9b
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-17 15:38:37 +0000

    r   extract method

--- SudokuKata/SudokuKata/Program.cs
+++ SudokuKata/SudokuKata/Program.cs
@@ -877,18 +877,24 @@ namespace SudokuKata
                 #region Print the board as it looks after one change was made to it
 
                 Console.WriteLine(sudokuBoardAndStackState);
-                string code =
-                    string.Join(string.Empty, sudokuBoardAndStackState.Board.Select(s => new string(s)).ToArray())
-                        .Replace("-", string.Empty)
-                        .Replace("+", string.Empty)
-                        .Replace("|", string.Empty)
-                        .Replace(".", "0");
+                var code = ToCodeString(sudokuBoardAndStackState);
 
                 Console.WriteLine("Code: {0}", code);
                 Console.WriteLine();
 
                 #endregion
             }
+        }
+
+        private static string ToCodeString(SudokuBoardAndStackState sudokuBoardAndStackState)
+        {
+            string code =
+                string.Join(string.Empty, sudokuBoardAndStackState.Board.Select(s => new string(s)).ToArray())
+                    .Replace("-", string.Empty)
+                    .Replace("+", string.Empty)
+                    .Replace("|", string.Empty)
+                    .Replace(".", "0");
+            return code;
         }
 
         public static void Main(string[] args)
