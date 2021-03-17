efc244c8c1058679542e8fd7042415e62e68e54d
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-17 16:09:38 +0000

    R   use SetValueWithoutCruft

--- SudokuKata/SudokuKata/Program.cs
+++ SudokuKata/SudokuKata/Program.cs
@@ -140,7 +140,7 @@ namespace SudokuKata
                         int colToWrite = col + col / 3 + 1;
 
                         boardAsNumbers[singleCandidateIndex] = candidate + 1;
-                        sudokuBoardAndStackState.SetValue(rowToWrite, colToWrite, 1+candidate);
+                        sudokuBoardAndStackState.SetValueWithoutCruft(row, col, 1+candidate);
                         candidateMasks[singleCandidateIndex] = 0;
                         wasChangeMade = true;
 
