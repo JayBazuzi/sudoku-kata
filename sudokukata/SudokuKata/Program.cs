64ba3eca61c1c3c3b211763a6fb9cae0817443c3
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-04-07 15:56:36 +0000

    r   extract method

--- sudokukata/SudokuKata/Program.cs
+++ sudokukata/SudokuKata/Program.cs
@@ -78,45 +78,7 @@ namespace SudokuKata
 
                 var candidateMasks = CalculateCandidatesForCurrentStateOfTheBoard(boardAsNumbers, allOnes).ReturnValue;
 
-                #region Build a collection (named cellGroups) which maps cell indices into distinct groups (rows/columns/blocks)
-
-                var rowsIndices = boardAsNumbers
-                    .Select((value, index) => new Applesauce1
-                    {
-                        Discriminator = index / 9, Description = $"row #{index / 9 + 1}", Index = index,
-                        Row = index / 9,
-                        Column = index % 9
-                    })
-                    .GroupBy(tuple => tuple.Discriminator);
-
-                var columnIndices = boardAsNumbers
-                    .Select((value, index) => new Applesauce1
-                    {
-                        Discriminator = 9 + index % 9, Description = $"column #{index % 9 + 1}", Index = index,
-                        Row = index / 9,
-                        Column = index % 9
-                    })
-                    .GroupBy(tuple => tuple.Discriminator);
-
-                var blockIndices = boardAsNumbers
-                    .Select((value, index) => new
-                    {
-                        Row = index / 9,
-                        Column = index % 9,
-                        Index = index
-                    })
-                    .Select(tuple => new Applesauce1
-                    {
-                        Discriminator = 18 + 3 * (tuple.Row / 3) + tuple.Column / 3,
-                        Description = $"block ({tuple.Row / 3 + 1}, {tuple.Column / 3 + 1})", Index = tuple.Index,
-                        Row = tuple.Row,
-                        Column = tuple.Column
-                    })
-                    .GroupBy(tuple => tuple.Discriminator);
-
-                var cellGroups = rowsIndices.Concat(columnIndices).Concat(blockIndices).ToList();
-
-                #endregion
+                var cellGroups = BuildCellGroups(boardAsNumbers);
 
                 var stepChangeMade = true;
                 while (stepChangeMade)
@@ -330,6 +292,51 @@ namespace SudokuKata
 
                 PrintBoardChange(wasChangeMade, puzzle);
             }
+        }
+
+        private static List<IGrouping<int, Applesauce1>> BuildCellGroups(int[] boardAsNumbers)
+        {
+            #region Build a collection (named cellGroups) which maps cell indices into distinct groups (rows/columns/blocks)
+
+            var rowsIndices = boardAsNumbers
+                .Select((value, index) => new Applesauce1
+                {
+                    Discriminator = index / 9, Description = $"row #{index / 9 + 1}", Index = index,
+                    Row = index / 9,
+                    Column = index % 9
+                })
+                .GroupBy(tuple => tuple.Discriminator);
+
+            var columnIndices = boardAsNumbers
+                .Select((value, index) => new Applesauce1
+                {
+                    Discriminator = 9 + index % 9, Description = $"column #{index % 9 + 1}", Index = index,
+                    Row = index / 9,
+                    Column = index % 9
+                })
+                .GroupBy(tuple => tuple.Discriminator);
+
+            var blockIndices = boardAsNumbers
+                .Select((value, index) => new
+                {
+                    Row = index / 9,
+                    Column = index % 9,
+                    Index = index
+                })
+                .Select(tuple => new Applesauce1
+                {
+                    Discriminator = 18 + 3 * (tuple.Row / 3) + tuple.Column / 3,
+                    Description = $"block ({tuple.Row / 3 + 1}, {tuple.Column / 3 + 1})", Index = tuple.Index,
+                    Row = tuple.Row,
+                    Column = tuple.Column
+                })
+                .GroupBy(tuple => tuple.Discriminator);
+
+            var cellGroups = rowsIndices.Concat(columnIndices).Concat(blockIndices).ToList();
+
+            #endregion
+
+            return cellGroups;
         }
 
         private static bool PickCellsWithOnlyOneCandidateRemaining(Random rng, SudokuBoard puzzle, int[] candidateMasks,
