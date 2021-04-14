ab98b49be39b3ebd1ad59215db19bb01c9dd83d4
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-04-14 16:07:26 +0000

    r   inline variable

--- SudokuKata/SudokuKata/Candidates.cs
+++ SudokuKata/SudokuKata/Candidates.cs
@@ -16,24 +16,22 @@ namespace SudokuKata
 
         public Cell[] GetCellsWithOnlyOneCandidateRemaining()
         {
-            var singleCandidateIndices =
-                Board
-                    .Select((mask, index) => new
-                    {
-                        CandidatesCount = _lookupStructures._maskToOnesCount[mask],
-                        Index = index,
-                        candidateMask = mask,
-                    })
-                    .Where(tuple => tuple.CandidatesCount == 1)
-                    .Select(tuple => new
-                    {
-                        Index = tuple.Index,
-                        candidate = _lookupStructures._singleBitToIndex[tuple.candidateMask]
-                            ,
-                    })
-                    .Select(tuple => Cell.FromIndex(tuple.Index, tuple.candidate + 1))
-                    .ToArray();
-            return singleCandidateIndices;
+            return Board
+                .Select((mask, index) => new
+                {
+                    CandidatesCount = _lookupStructures._maskToOnesCount[mask],
+                    Index = index,
+                    candidateMask = mask,
+                })
+                .Where(tuple => tuple.CandidatesCount == 1)
+                .Select(tuple => new
+                {
+                    Index = tuple.Index,
+                    candidate = _lookupStructures._singleBitToIndex[tuple.candidateMask]
+                    ,
+                })
+                .Select(tuple => Cell.FromIndex(tuple.Index, tuple.candidate + 1))
+                .ToArray();
         }
     }
 }
