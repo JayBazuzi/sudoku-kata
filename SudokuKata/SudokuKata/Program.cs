b382b8f1f74c57147abc673c99e0fcd8e00aa208
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-04-21 15:36:30 +0000

    r   remove braces

--- SudokuKata/SudokuKata/Program.cs
+++ SudokuKata/SudokuKata/Program.cs
@@ -197,101 +197,99 @@ namespace SudokuKata
             bool wasChangeMade = false;
             var candidateMasks = puzzle.GetCandidates().Board;
             var boardAsNumbers = puzzle.GetBoardAsNumbers();
+            var groupDescriptions = new List<string>();
+            var candidateRowIndices = new List<int>();
+            var candidateColIndices = new List<int>();
+            var candidates = new List<int>();
+
+            for (var digit = 1; digit <= 9; digit++)
             {
-                var groupDescriptions = new List<string>();
-                var candidateRowIndices = new List<int>();
-                var candidateColIndices = new List<int>();
-                var candidates = new List<int>();
-
-                for (var digit = 1; digit <= 9; digit++)
+                var mask = 1 << (digit - 1);
+                for (var cellGroup = 0; cellGroup < 9; cellGroup++)
                 {
-                    var mask = 1 << (digit - 1);
-                    for (var cellGroup = 0; cellGroup < 9; cellGroup++)
+                    var rowNumberCount = 0;
+                    var indexInRow = 0;
+
+                    var colNumberCount = 0;
+                    var indexInCol = 0;
+
+                    var blockNumberCount = 0;
+                    var indexInBlock = 0;
+
+                    for (var indexInGroup = 0; indexInGroup < 9; indexInGroup++)
                     {
-                        var rowNumberCount = 0;
-                        var indexInRow = 0;
+                        var rowStateIndex = 9 * cellGroup + indexInGroup;
+                        var colStateIndex = 9 * indexInGroup + cellGroup;
+                        var blockRowIndex = cellGroup / 3 * 3 + indexInGroup / 3;
+                        var blockColIndex = cellGroup % 3 * 3 + indexInGroup % 3;
+                        var blockStateIndex = blockRowIndex * 9 + blockColIndex;
 
-                        var colNumberCount = 0;
-                        var indexInCol = 0;
-
-                        var blockNumberCount = 0;
-                        var indexInBlock = 0;
-
-                        for (var indexInGroup = 0; indexInGroup < 9; indexInGroup++)
+                        if ((candidateMasks[rowStateIndex] & mask) != 0)
                         {
-                            var rowStateIndex = 9 * cellGroup + indexInGroup;
-                            var colStateIndex = 9 * indexInGroup + cellGroup;
-                            var blockRowIndex = cellGroup / 3 * 3 + indexInGroup / 3;
-                            var blockColIndex = cellGroup % 3 * 3 + indexInGroup % 3;
-                            var blockStateIndex = blockRowIndex * 9 + blockColIndex;
-
-                            if ((candidateMasks[rowStateIndex] & mask) != 0)
-                            {
-                                rowNumberCount += 1;
-                                indexInRow = indexInGroup;
-                            }
-
-                            if ((candidateMasks[colStateIndex] & mask) != 0)
-                            {
-                                colNumberCount += 1;
-                                indexInCol = indexInGroup;
-                            }
-
-                            if ((candidateMasks[blockStateIndex] & mask) != 0)
-                            {
-                                blockNumberCount += 1;
-                                indexInBlock = indexInGroup;
-                            }
+                            rowNumberCount += 1;
+                            indexInRow = indexInGroup;
                         }
 
-                        if (rowNumberCount == 1)
+                        if ((candidateMasks[colStateIndex] & mask) != 0)
                         {
-                            groupDescriptions.Add($"Row #{cellGroup + 1}");
-                            candidateRowIndices.Add(cellGroup);
-                            candidateColIndices.Add(indexInRow);
-                            candidates.Add(digit);
+                            colNumberCount += 1;
+                            indexInCol = indexInGroup;
                         }
 
-                        if (colNumberCount == 1)
+                        if ((candidateMasks[blockStateIndex] & mask) != 0)
                         {
-                            groupDescriptions.Add($"Column #{cellGroup + 1}");
-                            candidateRowIndices.Add(indexInCol);
-                            candidateColIndices.Add(cellGroup);
-                            candidates.Add(digit);
+                            blockNumberCount += 1;
+                            indexInBlock = indexInGroup;
                         }
+                    }
 
-                        if (blockNumberCount == 1)
-                        {
-                            var blockRow = cellGroup / 3;
-                            var blockCol = cellGroup % 3;
+                    if (rowNumberCount == 1)
+                    {
+                        groupDescriptions.Add($"Row #{cellGroup + 1}");
+                        candidateRowIndices.Add(cellGroup);
+                        candidateColIndices.Add(indexInRow);
+                        candidates.Add(digit);
+                    }
 
-                            groupDescriptions.Add($"Block ({blockRow + 1}, {blockCol + 1})");
-                            candidateRowIndices.Add(blockRow * 3 + indexInBlock / 3);
-                            candidateColIndices.Add(blockCol * 3 + indexInBlock % 3);
-                            candidates.Add(digit);
-                        }
-                    } // for (cellGroup = 0..8)
-                } // for (digit = 1..9)
+                    if (colNumberCount == 1)
+                    {
+                        groupDescriptions.Add($"Column #{cellGroup + 1}");
+                        candidateRowIndices.Add(indexInCol);
+                        candidateColIndices.Add(cellGroup);
+                        candidates.Add(digit);
+                    }
 
-                if (candidates.Count > 0)
-                {
-                    var index = rng.Next(candidates.Count);
-                    var description = groupDescriptions.ElementAt(index);
-                    var row = candidateRowIndices.ElementAt(index);
-                    var col = candidateColIndices.ElementAt(index);
-                    var digit = candidates.ElementAt(index);
+                    if (blockNumberCount == 1)
+                    {
+                        var blockRow = cellGroup / 3;
+                        var blockCol = cellGroup % 3;
 
-                    var message = $"{description} can contain {digit} only at ({row + 1}, {col + 1}).";
+                        groupDescriptions.Add($"Block ({blockRow + 1}, {blockCol + 1})");
+                        candidateRowIndices.Add(blockRow * 3 + indexInBlock / 3);
+                        candidateColIndices.Add(blockCol * 3 + indexInBlock % 3);
+                        candidates.Add(digit);
+                    }
+                } // for (cellGroup = 0..8)
+            } // for (digit = 1..9)
 
-                    var stateIndex = 9 * row + col;
-                    boardAsNumbers[stateIndex] = digit;
-                    candidateMasks[stateIndex] = 0;
-                    puzzle.SetValue(row, col, digit);
+            if (candidates.Count > 0)
+            {
+                var index = rng.Next(candidates.Count);
+                var description = groupDescriptions.ElementAt(index);
+                var row = candidateRowIndices.ElementAt(index);
+                var col = candidateColIndices.ElementAt(index);
+                var digit = candidates.ElementAt(index);
 
-                    wasChangeMade = true;
+                var message = $"{description} can contain {digit} only at ({row + 1}, {col + 1}).";
 
-                    Console.WriteLine(message);
-                }
+                var stateIndex = 9 * row + col;
+                boardAsNumbers[stateIndex] = digit;
+                candidateMasks[stateIndex] = 0;
+                puzzle.SetValue(row, col, digit);
+
+                wasChangeMade = true;
+
+                Console.WriteLine(message);
             }
 
             return wasChangeMade;
