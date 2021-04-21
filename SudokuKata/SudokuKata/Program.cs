dbd0fd204a4334c63c0a6c32efdc4aaea312cca2
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-04-21 15:28:30 +0000

    r   remove redundant

--- SudokuKata/SudokuKata/Program.cs
+++ SudokuKata/SudokuKata/Program.cs
@@ -349,8 +349,7 @@ namespace SudokuKata
 
         private static bool PickCellsWithOnlyOneCandidateRemaining(Random rng, SudokuBoard puzzle, Candidates candidates3)
         {
-            Candidates candidates = candidates3;
-            candidates = puzzle.GetCandidates();
+            var candidates = puzzle.GetCandidates();
             var singleCandidateIndices = candidates.GetCellsWithOnlyOneCandidateRemaining();
 
             var skip = singleCandidateIndices.Length == 0 ? 0 : rng.Next(singleCandidateIndices.Length);
