b5c3abd31357c5c87538413bc2b626855a9a9280
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-04-21 15:16:13 +0000

    r   invert if

--- SudokuKata/SudokuKata/Program.cs
+++ SudokuKata/SudokuKata/Program.cs
@@ -94,90 +94,92 @@ namespace SudokuKata
         {
             bool stepChangeMade = false;
 
-            if (!wasChangeMade)
+            if (wasChangeMade)
             {
-                IEnumerable<int> twoDigitMasks =
-                    candidateMasks.Where(mask => maskToOnesCount[mask] == 2).Distinct().ToList();
+                return stepChangeMade;
+            }
 
-                var groups =
-                    twoDigitMasks
-                        .SelectMany(mask =>
-                            cellGroups
-                                .Where(@group => @group.Count(tuple => candidateMasks[tuple.Index] == mask) == 2)
-                                .Where(@group => @group.Any(tuple =>
-                                    candidateMasks[tuple.Index] != mask &&
-                                    (candidateMasks[tuple.Index] & mask) > 0))
-                                .Select(@group => new Applesauce2
-                                {
-                                    Mask = mask, Discriminator = @group.Key,
-                                    Description = @group.First().Description,
-                                    Cells = @group
-                                }))
-                        .ToList();
+            IEnumerable<int> twoDigitMasks =
+                candidateMasks.Where(mask => maskToOnesCount[mask] == 2).Distinct().ToList();
 
-                if (groups.Any())
+            var groups =
+                twoDigitMasks
+                    .SelectMany(mask =>
+                        cellGroups
+                            .Where(@group => @group.Count(tuple => candidateMasks[tuple.Index] == mask) == 2)
+                            .Where(@group => @group.Any(tuple =>
+                                candidateMasks[tuple.Index] != mask &&
+                                (candidateMasks[tuple.Index] & mask) > 0))
+                            .Select(@group => new Applesauce2
+                            {
+                                Mask = mask, Discriminator = @group.Key,
+                                Description = @group.First().Description,
+                                Cells = @group
+                            }))
+                    .ToList();
+
+            if (groups.Any())
+            {
+                foreach (var @group in groups)
                 {
-                    foreach (var @group in groups)
+                    var cells =
+                        @group.Cells
+                            .Where(
+                                cell =>
+                                    candidateMasks[cell.Index] != @group.Mask &&
+                                    (candidateMasks[cell.Index] & @group.Mask) > 0)
+                            .ToList();
+
+                    var maskCells =
+                        @group.Cells
+                            .Where(cell => candidateMasks[cell.Index] == @group.Mask)
+                            .ToArray();
+
+
+                    if (cells.Any())
                     {
-                        var cells =
-                            @group.Cells
-                                .Where(
-                                    cell =>
-                                        candidateMasks[cell.Index] != @group.Mask &&
-                                        (candidateMasks[cell.Index] & @group.Mask) > 0)
-                                .ToList();
+                        var upper = 0;
+                        var lower = 0;
+                        var temp = @group.Mask;
 
-                        var maskCells =
-                            @group.Cells
-                                .Where(cell => candidateMasks[cell.Index] == @group.Mask)
-                                .ToArray();
-
-
-                        if (cells.Any())
+                        var value = 1;
+                        while (temp > 0)
                         {
-                            var upper = 0;
-                            var lower = 0;
-                            var temp = @group.Mask;
-
-                            var value = 1;
-                            while (temp > 0)
+                            if ((temp & 1) > 0)
                             {
-                                if ((temp & 1) > 0)
-                                {
-                                    lower = upper;
-                                    upper = value;
-                                }
-
-                                temp = temp >> 1;
-                                value += 1;
+                                lower = upper;
+                                upper = value;
                             }
 
+                            temp = temp >> 1;
+                            value += 1;
+                        }
+
+                        Console.WriteLine(
+                            $"Values {lower} and {upper} in {@group.Description} are in cells ({maskCells[0].Row + 1}, {maskCells[0].Column + 1}) and ({maskCells[1].Row + 1}, {maskCells[1].Column + 1}).");
+
+                        foreach (var cell in cells)
+                        {
+                            var maskToRemove = candidateMasks[cell.Index] & @group.Mask;
+                            var valuesToRemove = new List<int>();
+                            var curValue = 1;
+                            while (maskToRemove > 0)
+                            {
+                                if ((maskToRemove & 1) > 0)
+                                {
+                                    valuesToRemove.Add(curValue);
+                                }
+
+                                maskToRemove = maskToRemove >> 1;
+                                curValue += 1;
+                            }
+
+                            var valuesReport = string.Join(", ", valuesToRemove.ToArray());
                             Console.WriteLine(
-                                $"Values {lower} and {upper} in {@group.Description} are in cells ({maskCells[0].Row + 1}, {maskCells[0].Column + 1}) and ({maskCells[1].Row + 1}, {maskCells[1].Column + 1}).");
+                                $"{valuesReport} cannot appear in ({cell.Row + 1}, {cell.Column + 1}).");
 
-                            foreach (var cell in cells)
-                            {
-                                var maskToRemove = candidateMasks[cell.Index] & @group.Mask;
-                                var valuesToRemove = new List<int>();
-                                var curValue = 1;
-                                while (maskToRemove > 0)
-                                {
-                                    if ((maskToRemove & 1) > 0)
-                                    {
-                                        valuesToRemove.Add(curValue);
-                                    }
-
-                                    maskToRemove = maskToRemove >> 1;
-                                    curValue += 1;
-                                }
-
-                                var valuesReport = string.Join(", ", valuesToRemove.ToArray());
-                                Console.WriteLine(
-                                    $"{valuesReport} cannot appear in ({cell.Row + 1}, {cell.Column + 1}).");
-
-                                candidateMasks[cell.Index] &= ~@group.Mask;
-                                stepChangeMade = true;
-                            }
+                            candidateMasks[cell.Index] &= ~@group.Mask;
+                            stepChangeMade = true;
                         }
                     }
                 }
