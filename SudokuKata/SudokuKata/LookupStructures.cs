045811be0b88fb19b5cc3dfe100e5a1266b74b68
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-04-28 16:20:29 +0000

    R   move code

--- SudokuKata/SudokuKata/LookupStructures.cs
+++ SudokuKata/SudokuKata/LookupStructures.cs
@@ -7,9 +7,26 @@ namespace SudokuKata
         public Dictionary<int, int> _maskToOnesCount;
         public Dictionary<int, int> _singleBitToIndex;
 
-        public LookupStructures(Dictionary<int, int> singleBitToIndex,
-            Dictionary<int, int> maskToOnesCount)
+        public LookupStructures()
         {
+            #region Prepare lookup structures that will be used in further execution
+
+            var maskToOnesCount = new Dictionary<int, int>();
+            maskToOnesCount[0] = 0;
+            for (var i = 1; i < 1 << 9; i++)
+            {
+                var smaller = i >> 1;
+                var increment = i & 1;
+                maskToOnesCount[i] = maskToOnesCount[smaller] + increment;
+            }
+
+            var singleBitToIndex = new Dictionary<int, int>();
+            for (var i = 0; i < 9; i++)
+            {
+                singleBitToIndex[1 << i] = i;
+            }
+
+            #endregion
             _singleBitToIndex = singleBitToIndex;
             _maskToOnesCount = maskToOnesCount;
         }
--- SudokuKata/SudokuKata/Program.cs
+++ SudokuKata/SudokuKata/Program.cs
@@ -76,26 +76,9 @@ namespace SudokuKata
 
         public static LookupStructures PrepareLookupStructures()
         {
-            #region Prepare lookup structures that will be used in further execution
+ 
 
-            var maskToOnesCount = new Dictionary<int, int>();
-            maskToOnesCount[0] = 0;
-            for (var i = 1; i < 1 << 9; i++)
-            {
-                var smaller = i >> 1;
-                var increment = i & 1;
-                maskToOnesCount[i] = maskToOnesCount[smaller] + increment;
-            }
-
-            var singleBitToIndex = new Dictionary<int, int>();
-            for (var i = 0; i < 9; i++)
-            {
-                singleBitToIndex[1 << i] = i;
-            }
-
-            #endregion
-
-            return new LookupStructures(singleBitToIndex, maskToOnesCount);
+            return new LookupStructures();
         }
 
         private static void PrintLineOfEquals()
