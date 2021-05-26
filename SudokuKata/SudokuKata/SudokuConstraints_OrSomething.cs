e85d180a4acd2800a0befef09894421cc1eee3dd
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-05-26 16:08:02 +0000

    R   delete dead code

--- SudokuKata/SudokuKata/SudokuConstraints_OrSomething.cs
+++ SudokuKata/SudokuKata/SudokuConstraints_OrSomething.cs
@@ -34,24 +34,5 @@ namespace SudokuKata
             return
                 $"{{ Description = {Description}, Index = {Index}, Row = {Row}, Column = {Column} }}";
         }
-
-        public override bool Equals(object value)
-        {
-            var type = value as SudokuConstraints_OrSomething;
-            return type != null &&
-                   EqualityComparer<string>.Default.Equals(type.Description, Description) &&
-                   EqualityComparer<int>.Default.Equals(type.Index, Index) &&
-                   EqualityComparer<int>.Default.Equals(type.Row, Row) &&
-                   EqualityComparer<int>.Default.Equals(type.Column, Column);
-        }
-
-        public override int GetHashCode()
-        {
-            var num = 0x7a2f0b42;
-            num = -1521134295 * num + EqualityComparer<string>.Default.GetHashCode(Description);
-            num = -1521134295 * num + EqualityComparer<int>.Default.GetHashCode(Index);
-            num = -1521134295 * num + EqualityComparer<int>.Default.GetHashCode(Row);
-            return -1521134295 * num + EqualityComparer<int>.Default.GetHashCode(Column);
-        }
     }
 }
