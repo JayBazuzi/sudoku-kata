ddb1508dc5f8a58ebb13fd4e3bc7e7bf24ecc1cd
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-06-02 15:29:41 +0000

    r   extract var

--- SudokuKata/SudokuKata/SudokuSolverSteps/TryToFindPairsOfDigitsInTheSameRowColumnBlockAndRemoveThemFromOtherCollidingCells.cs
+++ SudokuKata/SudokuKata/SudokuSolverSteps/TryToFindPairsOfDigitsInTheSameRowColumnBlockAndRemoveThemFromOtherCollidingCells.cs
@@ -42,69 +42,76 @@ namespace SudokuKata
             {
                 var cellWithDescriptions = @group.Cells;
                 var mask = @group.Mask;
-                var cells =
-                    cellWithDescriptions
-                        .Where(
-                            cell =>
-                                candidateMasks[cell.Index] != mask &&
-                                SudokuBoard.IsDigitPossible(candidateMasks, mask, cell.Index))
-                        .ToList();
-
-                var maskCells =
-                    cellWithDescriptions
-                        .Where(cell => candidateMasks[cell.Index] == mask)
-                        .ToArray();
-
-
-                if (cells.Any())
-                {
-                    var upper = 0;
-                    var lower = 0;
-                    var temp = mask;
-
-                    var value = 1;
-                    while (temp != 0)
-                    {
-                        if ((temp & 1) != 0)
-                        {
-                            lower = upper;
-                            upper = value;
-                        }
-
-                        temp = temp >> 1;
-                        value += 1;
-                    }
-
-                    Console.WriteLine(
-                        $"Values {lower} and {upper} in {cellWithDescriptions.First().Description} are in cells ({maskCells[0].Row + 1}, {maskCells[0].Column + 1}) and ({maskCells[1].Row + 1}, {maskCells[1].Column + 1}).");
-
-                    foreach (var cell in cells)
-                    {
-                        var maskToRemove = candidateMasks[cell.Index] & mask;
-                        var valuesToRemove = new List<int>();
-                        var curValue = 1;
-                        while (maskToRemove != 0)
-                        {
-                            if ((maskToRemove & 1) != 0)
-                            {
-                                valuesToRemove.Add(curValue);
-                            }
-
-                            maskToRemove = maskToRemove >> 1;
-                            curValue += 1;
-                        }
-
-                        var valuesReport = string.Join(", ", valuesToRemove.ToArray());
-                        Console.WriteLine(
-                            $"{valuesReport} cannot appear in ({cell.Row + 1}, {cell.Column + 1}).");
-
-                        candidateMasks[cell.Index] &= ~mask;
-                        stepChangeMade = true;
-                    }
-                }
+                stepChangeMade = Applesauce(cellWithDescriptions, candidateMasks, mask, stepChangeMade);
             }
 
             return new ChangesMadeStates {CandidateChanged = stepChangeMade};
+        }
+
+        private static bool Applesauce(List<CellWithDescription> cellWithDescriptions, int[] candidateMasks, int mask, bool stepChangeMade)
+        {
+            var cells =
+                cellWithDescriptions
+                    .Where(
+                        cell =>
+                            candidateMasks[cell.Index] != mask &&
+                            SudokuBoard.IsDigitPossible(candidateMasks, mask, cell.Index))
+                    .ToList();
+
+            var maskCells =
+                cellWithDescriptions
+                    .Where(cell => candidateMasks[cell.Index] == mask)
+                    .ToArray();
+
+
+            if (cells.Any())
+            {
+                var upper = 0;
+                var lower = 0;
+                var temp = mask;
+
+                var value = 1;
+                while (temp != 0)
+                {
+                    if ((temp & 1) != 0)
+                    {
+                        lower = upper;
+                        upper = value;
+                    }
+
+                    temp = temp >> 1;
+                    value += 1;
+                }
+
+                Console.WriteLine(
+                    $"Values {lower} and {upper} in {cellWithDescriptions.First().Description} are in cells ({maskCells[0].Row + 1}, {maskCells[0].Column + 1}) and ({maskCells[1].Row + 1}, {maskCells[1].Column + 1}).");
+
+                foreach (var cell in cells)
+                {
+                    var maskToRemove = candidateMasks[cell.Index] & mask;
+                    var valuesToRemove = new List<int>();
+                    var curValue = 1;
+                    while (maskToRemove != 0)
+                    {
+                        if ((maskToRemove & 1) != 0)
+                        {
+                            valuesToRemove.Add(curValue);
+                        }
+
+                        maskToRemove = maskToRemove >> 1;
+                        curValue += 1;
+                    }
+
+                    var valuesReport = string.Join(", ", valuesToRemove.ToArray());
+                    Console.WriteLine(
+                        $"{valuesReport} cannot appear in ({cell.Row + 1}, {cell.Column + 1}).");
+
+                    candidateMasks[cell.Index] &= ~mask;
+                    stepChangeMade = true;
+                }
+            }
+
+            return stepChangeMade;
         }
     }
 }
