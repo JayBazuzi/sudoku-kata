9dec6d8b4c1a6546e890d8aed92b17b0ab0a1b64
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-06-30 15:16:10 +0000

    R   use SingleCandidates

--- SudokuKata/SudokuKata/SudokuSolverSteps/PickCellsWithOnlyOneCandidateRemaining.cs
+++ SudokuKata/SudokuKata/SudokuSolverSteps/PickCellsWithOnlyOneCandidateRemaining.cs
@@ -10,24 +10,23 @@ namespace SudokuKata
         public ChangesMadeStates Do(Random rng, SudokuBoard puzzle)
         {
             // TODO: stop using GetCandidates here
-            var singleCandidateIndices = puzzle.GetCandidates().GetCellsWithOnlyOneCandidateRemaining();
+            //var singleCandidateIndices = puzzle.GetCandidates().GetCellsWithOnlyOneCandidateRemaining();
             var singleCandidates = puzzle.GetPossibilities()
                 .Select((possibilities, index) => new CellWithPossiblities(index, possibilities))
                 .Where(c => c.Possibilities.Count == 1)
                 .ToArray();
 
-            Debug.Assert(singleCandidates.Length == singleCandidateIndices.Length);
 
-            var skip = singleCandidateIndices.Length == 0 ? 0 : rng.Next(singleCandidateIndices.Length);
-            var cell = singleCandidateIndices.Skip(skip).FirstOrDefault();
+            var skip = singleCandidates.Length == 0 ? 0 : rng.Next(singleCandidates.Length);
+            var cell = singleCandidates.Skip(skip).FirstOrDefault();
             if (cell == null)
             {
                 return ChangesMadeStates.None;
             }
 
-            puzzle.SetValue(cell.Row, cell.Column, cell.Value);
+            puzzle.SetValue(cell.Cell.Row, cell.Cell.Column, cell.Possibilities.First());
 
-            Console.WriteLine("({0}, {1}) can only contain {2}.", cell.Row + 1, cell.Column + 1, cell.Value);
+            Console.WriteLine("({0}, {1}) can only contain {2}.", cell.Cell.Row + 1, cell.Cell.Column + 1, cell.Possibilities.First());
 
             return new ChangesMadeStates {CellChanged = true};
         }
